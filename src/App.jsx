185dd047a890224e922d1cabd7a7b1d9f36b3aee
Alvin Kigen <cartezalvin@gmail.com>
2023-02-23 10:34:04 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from'./components/about/About'
+import Experience from'./components/experience/Experience'
+import Services from'./components/services/Services'
+import Portfolio from'./components/portfolio/Portfolio'
+import Testimonials from'./components/testimonials/Testimonials'
+import Contact from'./components/contact/Contact'
+import Footer from'./components/footer/Footer'
+
+// function App() {
+//   return (
+//     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+//       <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
+//       <p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Testimonials() {
+  return (
+    <div className="text-blue-500">Testimonials</div>
+  )
+}
+
+export default Testimonials
