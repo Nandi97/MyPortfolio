48d7c1a6505ebefc38fe2a116497f06e5283ce16
Alvin Kigen <cartezalvin@gmail.com>
2023-03-16 13:12:58 +0300

    added favicon

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import {AiOutlineMail} from 'react-icons/ai'
-import {RiMessengerLine} from 'react-icons/ri'
-import {BsWhatsapp} from 'react-icons/bs'
+import { AiOutlineMail } from 'react-icons/ai'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
 const Contact = () => {
     return (
         <section id="contact">
@@ -11,27 +11,29 @@ const Contact = () => {
             <div className="container contact__container">
                 <div className="contact__options">
                     <article className="contact__option">
-                        <AiOutlineMail/>
+                        <AiOutlineMail />
                         <h4>Email</h4>
                         <h5>alvin_kigen@yahoo.com</h5>
-                        <a href="mailto:alvin_kigen@yahoo.com">Send a Message</a>
-                        
-</article>
+                        <a href="mailto:alvin_kigen@yahoo.com">
+                            Send a Message
+                        </a>
+                    </article>
                     <article className="contact__option">
-                        <RiMessengerLine/>
+                        <RiMessengerLine />
                         <h4>Messenger</h4>
                         <h5>alvin kigen</h5>
-                        <a href="https://m.me/cartez.detwiler1">Send a Message</a>
-                        
-</article>
+                        <a href="https://m.me/cartez.detwiler1">
+                            Send a Message
+                        </a>
+                    </article>
                     <article className="contact__option">
-                        <BsWhatsapp/>
+                        <BsWhatsapp />
                         <h4>Whatsapp</h4>
                         <h5>+254-712-012-246</h5>
-                        <a href="mailto:alvin_kigen@yahoo.com">Send a Message</a>
-                        
-</article>
-
+                        <a href="mailto:alvin_kigen@yahoo.com">
+                            Send a Message
+                        </a>
+                    </article>
                 </div>
                 {/* END  */}
                 <form action=""></form>
